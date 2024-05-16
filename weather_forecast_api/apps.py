624a4bbb05403b45e47a99407cac444b16559e14
Krystian <krystiano1602@gmail.com>
2024-05-16 14:06:06 +0200

    Revert "Project implemented."
    This reverts commit e3ca788386b3ff06318c1f22e9032725094d9bff.

--- weather_forecast_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WeatherForecastApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'weather_forecast_api'
--- weather_forecast_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', weather_api_post_method)
-]
